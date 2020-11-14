c489e2aa60bd4a9615f1422c5195a9f225a6fd06
Nidhi Gupta <nidhi.gupta_cs18@gla.ac.in>
2020-11-15 02:14:32 +0530

    javascript day3

---
+++ javascript_day3.js
@@ -0,0 +1,12 @@
+var x=10,y=2;
+y="nidhi";
+y=3.14;
+console.log(x);
+console.log(y);//variables can be used without using "var" keyword
+
+"use strict";//javascript has two modes-sloppy and strict,strict version uses var to declare keywords and does not support octal versions 
+//ECMAScript 5 uses strixt version of java script 
+z=20;//trying to declare variable without using var
+console.log(z);
+octa=0010;
+console.log(octa);
